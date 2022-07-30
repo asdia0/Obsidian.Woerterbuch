a258a1903967b6c26d6efb041dfc56f74ee8d540
asdia0 <asdia.the.potato@gmail.com>
2022-07-31 02:52:21 +0800

    Add documentation to Tense

--- Obsidian.Woerterbuch/Tense.cs
+++ Obsidian.Woerterbuch/Tense.cs
@@ -1,12 +1,38 @@
 ﻿namespace Obsidian.Woerterbuch
 {
+    /// <summary>
+    /// An enum of German tenses.
+    /// </summary>
     public enum Tense
     {
+        /// <summary>
+        /// Präsens.
+        /// </summary>
         Present,
+
+        /// <summary>
+        /// Perfekt.
+        /// </summary>
         Perfect,
+
+        /// <summary>
+        /// Präteritum.
+        /// </summary>
         SimplePast,
+
+        /// <summary>
+        /// Plusquamperfekt.
+        /// </summary>
         PastPerfect,
+
+        /// <summary>
+        /// Futur 1.
+        /// </summary>
         Futur1,
+
+        /// <summary>
+        /// Futur 2.
+        /// </summary>
         Futur2
     }
 }
