7e4ec382a8e0e5f72714c9769fc25b61f25393c0
asdia0 <asdia.the.potato@gmail.com>
2022-07-31 02:53:21 +0800

    Create Conjugation

--- Obsidian.Woerterbuch/Conjugation.cs
+++ Obsidian.Woerterbuch/Conjugation.cs
@@ -2,5 +2,15 @@
 {
     public class Conjugation
     {
+        public Tense Tense { get; set; }
+
+        public string[] Conjugations { get; set; }
+
+        public Conjugation(string infinitive, Tense tense)
+        {
+            this.Tense = tense;
+            this.Conjugations = new string[7];
+            this.Conjugations[0] = infinitive;
+        }
     }
 }
