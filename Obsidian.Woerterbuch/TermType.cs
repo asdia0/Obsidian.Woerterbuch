004792efa5a20f8d86838c3416ad52609251d5f2
asdia0 <asdia.the.potato@gmail.com>
2022-07-31 02:58:39 +0800

    Rename Class to TermType

---
+++ Obsidian.Woerterbuch/TermType.cs
@@ -0,0 +1,18 @@
+﻿namespace Obsidian.Woerterbuch
+{
+    /// <summary>
+    /// An enum of the different word classes.
+    /// </summary>
+    public enum TermType
+    {
+        Noun,
+        Verb,
+        Adjective,
+        Adverb,
+        Pronoun,
+        Preposition,
+        Conjunction,
+        Determiner,
+        Exclamation,
+    }
+}
