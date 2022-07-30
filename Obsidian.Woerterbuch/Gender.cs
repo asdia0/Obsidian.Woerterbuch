88e07446ee58f01c05d9baf3d5220fe5fd5cce4e
asdia0 <asdia.the.potato@gmail.com>
2022-07-31 02:39:27 +0800

    Add documentation to Gender

--- Obsidian.Woerterbuch/Gender.cs
+++ Obsidian.Woerterbuch/Gender.cs
@@ -1,9 +1,22 @@
 ﻿namespace Obsidian.Woerterbuch
-{
+{    /// <summary>
+     /// An enum of the genders for nouns.
+     /// </summary>
     public enum Gender
     {
+        /// <summary>
+        /// Masculine (der)
+        /// </summary>
         Maskulinum,
+
+        /// <summary>
+        /// Feminine (die)
+        /// </summary>
         Femininum,
+
+        /// <summary>
+        /// Neutral (das)
+        /// </summary>
         Neutrum
     }
 }
