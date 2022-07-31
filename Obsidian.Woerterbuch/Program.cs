4a6b4e2a820278327d909f0d9fa2cc54bc73ab02
asdia0 <asdia.the.potato@gmail.com>
2022-07-31 17:11:20 +0800

    Create Program.Dictionary

--- Obsidian.Woerterbuch/Program.cs
+++ Obsidian.Woerterbuch/Program.cs
@@ -1,9 +1,13 @@
 ﻿namespace Obsidian.Woerterbuch
 {
     using System;
+    using System.Collections.Generic;
+    using Dictionary;
 
     public static class Program
     {
+        public static List<Term> Dictionary;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
