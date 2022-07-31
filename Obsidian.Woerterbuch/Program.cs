e3e39847856d3809b24ccc85f63a1e27fa3e2196
asdia0 <asdia.the.potato@gmail.com>
2022-08-01 00:13:04 +0800

    Add newlines

--- Obsidian.Woerterbuch/Program.cs
+++ Obsidian.Woerterbuch/Program.cs
@@ -114,7 +114,7 @@
                 Console.Clear();
 
                 Console.WriteLine("Options:\nv <term> <type> - View term\na <term> <type> - Add term\ne <term> <type> - Edit term\nd <term> <type> - Delete term\n");
-                Console.WriteLine("Types:\n  * n - noun\n  * v - verb\n  * adv - adverb\n  * adj- adjective\n  * pro - pronoun\n  * preposition\n  * c - conjunction\n  * d - determiner\n  * e - exclamation");
+                Console.WriteLine("Types:\n  * n - noun\n  * v - verb\n  * adv - adverb\n  * adj- adjective\n  * pro - pronoun\n  * preposition\n  * c - conjunction\n  * d - determiner\n  * e - exclamation\n");
 
                 List<string> inputs = Console.ReadLine().Split(" ").ToList();
                 string option = inputs[0].ToLower();
@@ -287,13 +287,13 @@
             {
                 Console.WriteLine("  * akk <bool> - Is Akkusativ\n  * dat <bool> - Is Dativ");
             }
-            Console.WriteLine("Indexes start from 0.");
+            Console.WriteLine("\nIndexes start from 0.");
             Console.WriteLine("To add a new entry, set index to -1.");
-            Console.WriteLine("To delete an entry, set content to \"[REMOVE ENTRY]\".");
-            Console.WriteLine("Types:\n  * n - noun\n  * v - verb\n  * adv - adverb\n  * adj- adjective\n  * pro - pronoun\n  * preposition\n  * c - conjunction\n  * d - determiner\n  * e - exclamation");
-            Console.WriteLine("Gender:\n  * r - masculine (der)\n  * e - feminine (die)\n  * s - neutral (das)");
-            Console.WriteLine("Tense:\n  * pre - present (Präsens)\n  * per - perfect (Perfekt)\n  * sp - simple past (Präteritum)\n  * pp - past perfect (Plusquamperfekt)\n  * f1 - future 1 (Futur 1)\n  * f2 - future 2 (Futur 2)");
-            Console.WriteLine("Booleans\n  * 1 - true\n  * anything else - false");
+            Console.WriteLine("To delete an entry, set content to \"[REMOVE ENTRY]\".\n");
+            Console.WriteLine("Types:\n  * n - noun\n  * v - verb\n  * adv - adverb\n  * adj- adjective\n  * pro - pronoun\n  * preposition\n  * c - conjunction\n  * d - determiner\n  * e - exclamation\n");
+            Console.WriteLine("Gender:\n  * r - masculine (der)\n  * e - feminine (die)\n  * s - neutral (das)\n");
+            Console.WriteLine("Tense:\n  * pre - present (Präsens)\n  * per - perfect (Perfekt)\n  * sp - simple past (Präteritum)\n  * pp - past perfect (Plusquamperfekt)\n  * f1 - future 1 (Futur 1)\n  * f2 - future 2 (Futur 2)\n");
+            Console.WriteLine("Booleans\n  * 1 - true\n  * anything else - false\n");
 
             List<string> arguments = Console.ReadLine().Split(" ").ToList();
 
