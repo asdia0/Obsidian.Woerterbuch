318d04667d9c6f7a8df72b9e3383c2b549e7576a
asdia0 <asdia.the.potato@gmail.com>
2022-08-01 01:17:29 +0800

    Call InitializeDictionary() in Flashcard

--- Obsidian.Woerterbuch/Flashcard.cs
+++ Obsidian.Woerterbuch/Flashcard.cs
@@ -1,10 +1,12 @@
 ﻿namespace Obsidian.Woerterbuch.Flashcard
 {
+    using Dictionary;
+
     public static class Flashcard
     {
         public static void Generate()
         {
-            
+            Dictionary.IntializeDictionary();
         }
     }
 }
