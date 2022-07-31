83fb28df12d85e8ebf12e530704afde8a7941e32
asdia0 <asdia.the.potato@gmail.com>
2022-07-31 17:15:25 +0800

    Initialize Dictionary on start

--- Obsidian.Woerterbuch/Program.cs
+++ Obsidian.Woerterbuch/Program.cs
@@ -1,16 +1,28 @@
 ﻿namespace Obsidian.Woerterbuch
 {
+    using Dictionary;
     using System;
     using System.Collections.Generic;
-    using Dictionary;
+    using System.IO;
+    using Newtonsoft.Json;
 
     public static class Program
     {
+        public static string Path = "dict.json";
+
         public static List<Term> Dictionary;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (File.Exists(Path))
+            {
+                Dictionary = JsonConvert.DeserializeObject<List<Term>>(File.ReadAllText(Path));
+            }
+            else
+            {
+                File.Create(Path);
+                Dictionary = new();
+            }
         }
     }
 }
