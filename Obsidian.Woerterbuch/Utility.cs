e7d2b7cbf62ca266b7687321d0d093dfb282c9b1
asdia0 <asdia.the.potato@gmail.com>
2022-07-31 16:17:03 +0800

    Add Utility.Serialize

--- Obsidian.Woerterbuch/Utility.cs
+++ Obsidian.Woerterbuch/Utility.cs
@@ -1,6 +1,20 @@
 ﻿namespace Obsidian.Woerterbuch
 {
+    using Newtonsoft.Json;
+
     public static class Utility
     {
+        /// <summary>
+        /// Serializes an object.
+        /// </summary>
+        /// <param name="value">The object to serialize.</param>
+        /// <returns>A JSON string of the object.</returns>
+        public static string Serialize(object? value)
+        {
+            var jsonSerializerSettings = new JsonSerializerSettings();
+            jsonSerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+            jsonSerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            return JsonConvert.SerializeObject(value, jsonSerializerSettings);
+        }
     }
 }
