b959e4a98d1cc56270a3e5e9d7a8567d567b5fc9
asdia0 <asdia.the.potato@gmail.com>
2022-08-07 21:05:20 +0800

    Add Global.SaveDictionary

--- Obsidian.Woerterbuch.UI/Global.cs
+++ Obsidian.Woerterbuch.UI/Global.cs
@@ -2,10 +2,16 @@
 {
     using Dictionary;
     using System.Collections.ObjectModel;
+    using System.Linq;
 
     public static class Global
     {
         public static ObservableCollection<Term> Dictionary { get; set; }
 
+        public static void SaveDictionary()
+        {
+            Utility.Dictionary = Dictionary.ToList();
+            Utility.SaveDictionary();
+        }
     }
 }
