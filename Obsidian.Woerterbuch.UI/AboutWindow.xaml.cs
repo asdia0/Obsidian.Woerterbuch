b92c835549d06878165710c8ced7226055f4ae3f
asdia0 <asdia.the.potato@gmail.com>
2022-08-07 20:28:40 +0800

    Add AboutWindow

---
+++ Obsidian.Woerterbuch.UI/AboutWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Obsidian.Woerterbuch.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+            this.TextBox.Text = "Obsidian.Woerterbuch.UI\nv0.1\nCreated by Asdia0\nhttps://github.com/asdia0/Obsidian.Woerterbuch";
+        }
+    }
+}
