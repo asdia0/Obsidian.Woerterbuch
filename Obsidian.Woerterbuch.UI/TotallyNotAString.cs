d84da334129cd01f3244f6a15c660c11f3235240
asdia0 <asdia.the.potato@gmail.com>
2022-08-10 23:47:20 +0800

    Add hacky workaround

---
+++ Obsidian.Woerterbuch.UI/TotallyNotAString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsidian.Woerterbuch.UI
+{
+    using System.ComponentModel;
+
+    public class TotallyNotAString : INotifyPropertyChanged
+    {
+        private string _notAString;
+        public string NotAString
+        {
+            get { return _notAString; }
+            set { _notAString = value; OnPropertyChanged(nameof(NotAString)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public TotallyNotAString(string str)
+        {
+            this.NotAString = str;
+        }
+    }
+}
